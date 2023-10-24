c929324ef859b7612c9711a2a5384b16a76dfc1c
Adrian <adrhc@outlook.com>
2023-10-24 23:43:05 +0300

    IndexSearchServiceTest

--- src/test/java/ro/go/adrhc/persistence/lucene/index/person/IndexSearchServiceTest.java
+++ src/test/java/ro/go/adrhc/persistence/lucene/index/person/IndexSearchServiceTest.java
@@ -24,14 +24,14 @@ class IndexSearchServiceTest {
 
 		IndexSearchService<String, Person> searchService =
 				createSearchService(PersonQueryFactory::nameEquals, tmpDir);
-		List<Person> result = searchService.findAllMatches("aast");
+		List<Person> result = searchService.findAllMatches("cast");
 
 		assertThat(result).hasSize(2);
 	}
 
 	private static List<Person> createPeople() {
 		return List.of(
-				new Person("1", " IMG-20210725-WA0029 ccc_ddd aAșț.jpeg "),
-				new Person("2", "(Original)person222 aAșț"));
+				new Person("1", "IMG-20210725-WA0029 ccc_ddd CAșț.jpeg"),
+				new Person("2", "(Original)person222 CAșț"));
 	}
 }
