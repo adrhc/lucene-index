26e45683ac49565cf8a77c61550ba6a5f11b85d9
Adrian <adrhc@outlook.com>
2023-10-24 16:01:59 +0300

    TokenizationUtilsTest.java TokenizationUtilsTestFactory.java

--- src/test/java/ro/go/adrhc/persistence/lucene/index/core/TokenizationUtilsTest.java
+++ src/test/java/ro/go/adrhc/persistence/lucene/index/core/TokenizationUtilsTest.java
@@ -3,33 +3,21 @@ package ro.go.adrhc.persistence.lucene.index.core;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import ro.go.adrhc.persistence.lucene.index.core.analysis.AnalyzerFactory;
 import ro.go.adrhc.persistence.lucene.index.core.tokenizer.TokenizationUtils;
-import ro.go.adrhc.persistence.lucene.index.core.tokenizer.TokenizerProperties;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 class TokenizationUtilsTest {
-	private static final AnalyzerFactory ANALYZER_FACTORY =
-			new AnalyzerFactory(TokenizerProperties.of(2,
-					List.of(new String[]{"_", " "}, new String[]{"-", " "})));
-	private static final TokenizationUtils TOKENIZATION_UTILS =
-			new TokenizationUtils(ANALYZER_FACTORY);
+	private static final TokenizationUtils TOKENIZATION_UTILS = TokenizationUtilsTestFactory.create();
 
 	@Test
 	void tokenize() throws IOException {
-		Set<String> tokens = TOKENIZATION_UTILS.tokenize(" AaA x bBb aAa aAșț ttt.ttt ccc_ccc");
-		assertThat(tokens).contains("aaa", "bbb", "ccc", "aast", "ttt.ttt");
-	}
-
-	@Test
-	void tokenizeFilename() throws IOException {
-		Set<String> tokens = TOKENIZATION_UTILS.tokenize("IMG-20210725-WA0029");
-		assertThat(tokens).contains("img", "20210725", "wa0029");
+		Set<String> tokens = TOKENIZATION_UTILS.tokenize(
+				" IMG-20210725-WA0029 AaA x bBb aAa aAșț ttt.ttt ccc_ccc");
+		assertThat(tokens).contains("img", "20210725", "wa0029", "aaa", "bbb", "ccc", "aast", "ttt.ttt");
 	}
 }
