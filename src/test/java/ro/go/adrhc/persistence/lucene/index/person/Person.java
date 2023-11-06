04e9930bd1f92dd189fb285d2d6dd2405650efe1
Adrian <adrhc@outlook.com>
2023-11-06 20:47:54 +0200

    Person.java

--- src/test/java/ro/go/adrhc/persistence/lucene/index/person/Person.java
+++ src/test/java/ro/go/adrhc/persistence/lucene/index/person/Person.java
@@ -1,13 +1,11 @@
 package ro.go.adrhc.persistence.lucene.index.person;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import ro.go.adrhc.persistence.lucene.typedindex.core.docds.rawds.Identifiable;
 
 public record Person(Long id, String cnp, String name, String aliasKeyword,
 		String aliasWord, String aliasPhrase, Integer intField, Long longField,
 		String storedOnlyField) implements Identifiable<Long> {
 	@Override
-	@JsonIgnore
 	public Long getId() {
 		return id;
 	}
