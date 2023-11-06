ca33c3fdb8818a53569c2048d03e5ea001c72ac1
Adrian <adrhc@outlook.com>
2023-11-06 19:23:19 +0200

    removed rawidserde package

--- src/main/java/ro/go/adrhc/persistence/lucene/typedindex/core/docds/rawidserde/DefaultRawIdSerde.java
+++
@@ -1,7 +0,0 @@
-package ro.go.adrhc.persistence.lucene.typedindex.core.docds.rawidserde;
-
-public record DefaultRawIdSerde<ID>(
-		StringToRawIdConverter<ID> stringToRawIdConverter,
-		RawIdToStringConverter<ID> rawIdToStringConverter)
-		implements RawIdSerde<ID> {
-}
