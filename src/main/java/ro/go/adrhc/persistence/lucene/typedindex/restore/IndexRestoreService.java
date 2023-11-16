1171b6506b2c76b5240274d73f827ff6fe26821f
Adrian <adrhc@outlook.com>
2023-11-16 13:48:09 +0200

    IndexRestoreService.java

--- src/main/java/ro/go/adrhc/persistence/lucene/typedindex/restore/IndexRestoreService.java
+++ src/main/java/ro/go/adrhc/persistence/lucene/typedindex/restore/IndexRestoreService.java
@@ -2,6 +2,9 @@ package ro.go.adrhc.persistence.lucene.typedindex.restore;
 
 import java.io.IOException;
 
+/**
+ * Change the index to only have the IndexDataSource data; remove (by id) the surplus, add missing.
+ */
 public interface IndexRestoreService<ID, T> {
 	void restore(IndexDataSource<ID, T> dataSource) throws IOException;
 }
