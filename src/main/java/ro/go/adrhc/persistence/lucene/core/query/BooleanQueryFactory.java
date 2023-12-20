fe41736357d0f4d1c10596f8f2bc92306593d8f5
Adrian <adrhc@outlook.com>
2023-12-20 12:37:22 +0200

    BooleanQueryFactory.mustSatisfy

--- src/main/java/ro/go/adrhc/persistence/lucene/core/query/BooleanQueryFactory.java
+++ src/main/java/ro/go/adrhc/persistence/lucene/core/query/BooleanQueryFactory.java
@@ -7,6 +7,12 @@ import java.util.Collection;
 import java.util.stream.Stream;
 
 public class BooleanQueryFactory {
+    public static BooleanQuery mustSatisfy(Collection<? extends Query> queries) {
+        BooleanQueryBuilder builder = new BooleanQueryBuilder();
+        queries.forEach(builder::mustSatisfy);
+        return builder.build();
+    }
+
     public static BooleanQuery shouldSatisfy(Collection<? extends Query> queries) {
         BooleanQueryBuilder builder = new BooleanQueryBuilder();
         queries.forEach(builder::shouldSatisfy);
