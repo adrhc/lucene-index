23fecdaac3650900f5813aed03d8dc6381e08eab
adrian-petre <Adrian.Petre@mastercard.com>
2024-05-27 13:03:53 +0300

    FieldType comments

--- src/main/java/ro/go/adrhc/persistence/lucene/core/field/FieldType.java
+++ src/main/java/ro/go/adrhc/persistence/lucene/core/field/FieldType.java
@@ -4,7 +4,7 @@ public enum FieldType {
 	KEYWORD, // indexed as a single token, NOT normalized!
 	WORD, // indexed as a single token, normalized (aka, filtered)
 	PHRASE, // indexed as a many tokens, normalized
-	LONG,
+	LONG, // used to for Long and Instant
 	INT,
 	STORED
 }
