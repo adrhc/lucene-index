a4efd63ff6d524ce022e91237b371be298a7dbf4
Adrian <adrhc@outlook.com>
2024-05-31 02:33:41 +0300

    MatchAllDocsQuery

--- src/test/java/ro/go/adrhc/persistence/lucene/index/person/PersonSortTest.java
+++ src/test/java/ro/go/adrhc/persistence/lucene/index/person/PersonSortTest.java
@@ -1,7 +1,7 @@
 package ro.go.adrhc.persistence.lucene.index.person;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.lucene.search.FieldExistsQuery;
+import org.apache.lucene.search.MatchAllDocsQuery;
 import org.apache.lucene.search.Sort;
 import org.apache.lucene.search.SortedNumericSortField;
 import org.apache.lucene.search.SortedSetSortField;
@@ -15,7 +15,8 @@ import java.io.IOException;
 import static org.apache.lucene.search.SortField.Type.LONG;
 import static org.assertj.core.api.Assertions.assertThat;
 import static ro.go.adrhc.persistence.lucene.index.person.PeopleGenerator.generatePeopleList;
-import static ro.go.adrhc.persistence.lucene.index.person.PersonFieldType.*;
+import static ro.go.adrhc.persistence.lucene.index.person.PersonFieldType.cnp;
+import static ro.go.adrhc.persistence.lucene.index.person.PersonFieldType.instantField;
 
 @ExtendWith(MockitoExtension.class)
 @Slf4j
@@ -26,13 +27,13 @@ public class PersonSortTest extends AbstractPersonsIndexTest {
 
 		// 1st page
 		SortedValues<Person> page1 = indexRepository.findMany(
-				new FieldExistsQuery(id.name()), 10, sort);
+				new MatchAllDocsQuery(), 10, sort);
 		assertThat(page1.values()).hasSize(10);
 		assertThat(page1.values()).map(Person::id).containsSequence(0L, 28L, 1L);
 
 		// 2nd page
-		SortedValues<Person> page2 = indexRepository.findManyAfter(page1.last(),
-				new FieldExistsQuery(id.name()), 10, sort);
+		SortedValues<Person> page2 = indexRepository.findManyAfter(
+				page1.last(), new MatchAllDocsQuery(), 10, sort);
 		assertThat(page2.values()).hasSize(10);
 		assertThat(page2.values()).map(Person::id).containsSequence(5L, 33L, 6L);
 	}
@@ -41,7 +42,7 @@ public class PersonSortTest extends AbstractPersonsIndexTest {
 	void findManySortInstantField() throws IOException {
 		Sort sort = new Sort(new SortedNumericSortField(instantField.name(), LONG));
 		SortedValues<Person> result = indexRepository.findMany(
-				new FieldExistsQuery(id.name()), 10, sort);
+				new MatchAllDocsQuery(), 10, sort);
 		assertThat(result.values()).hasSize(10);
 		assertThat(result.values()).map(Person::id).containsSequence(0L, 28L, 1L);
 	}
@@ -50,7 +51,7 @@ public class PersonSortTest extends AbstractPersonsIndexTest {
 	void findManySortCnp() throws IOException {
 		Sort sort = new Sort(new SortedSetSortField(cnp.name(), false));
 		SortedValues<Person> result = indexRepository.findMany(
-				new FieldExistsQuery(id.name()), 10, sort);
+				new MatchAllDocsQuery(), 10, sort);
 		assertThat(result.values()).hasSize(10);
 		assertThat(result.values()).map(Person::id).containsSequence(0L, 1L, 10L);
 	}
