228c237573c4fd7e44b94d78020cb784a8368b79
Adrian <adrhc@outlook.com>
2024-05-31 18:47:21 +0300

    SortedValues.java

--- src/main/java/ro/go/adrhc/persistence/lucene/typedindex/search/SortedValues.java
+++ src/main/java/ro/go/adrhc/persistence/lucene/typedindex/search/SortedValues.java
@@ -4,12 +4,17 @@ import org.apache.lucene.search.ScoreDoc;
 import ro.go.adrhc.util.stream.StreamAware;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 public record SortedValues<T>(List<T> values, ScoreDoc first, ScoreDoc last)
 		implements StreamAware<T> {
 	public boolean isEmpty() {
 		return values.isEmpty();
+	}
+
+	public <Y> SortedValues<Y> map(Function<? super List<T>, List<Y>> mapper) {
+		return new SortedValues<>(mapper.apply(values), first, last);
 	}
 
 	@Override
