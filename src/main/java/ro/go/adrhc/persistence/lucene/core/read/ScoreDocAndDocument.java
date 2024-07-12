a5430126a28d5c74709698907220c13214a18e61
Adrian <adrhc@outlook.com>
2024-07-12 19:50:50 +0300

    ScoreDocAndDocument.getFieldValue

--- src/main/java/ro/go/adrhc/persistence/lucene/core/read/ScoreDocAndDocument.java
+++ src/main/java/ro/go/adrhc/persistence/lucene/core/read/ScoreDocAndDocument.java
@@ -4,7 +4,7 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.search.ScoreDoc;
 
 public record ScoreDocAndDocument(ScoreDoc scoreDoc, Document document) {
-	public String getField(Enum<?> field) {
+	public String getFieldValue(Enum<?> field) {
 		return document.get(field.name());
 	}
 }
