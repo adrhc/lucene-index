4f1c265b3c627b6233a348e0c3d1ed8b1bb2df75
Adrian <adrhc@outlook.com>
2024-07-16 15:10:10 +0300

    ScoreDocAndDocument(ScoreDoc scoreDoc, Document document) implements BrokenObject

--- src/main/java/ro/go/adrhc/persistence/lucene/core/bare/read/ScoreDocAndDocument.java
+++ src/main/java/ro/go/adrhc/persistence/lucene/core/bare/read/ScoreDocAndDocument.java
@@ -2,9 +2,15 @@ package ro.go.adrhc.persistence.lucene.core.bare.read;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.search.ScoreDoc;
+import ro.go.adrhc.util.specialcase.BrokenObject;
 
-public record ScoreDocAndDocument(ScoreDoc scoreDoc, Document document) {
+public record ScoreDocAndDocument(ScoreDoc scoreDoc, Document document) implements BrokenObject {
 	public String getFieldValue(Enum<?> field) {
 		return document.get(field.name());
+	}
+
+	@Override
+	public boolean isBroken() {
+		return scoreDoc == null || document == null;
 	}
 }
