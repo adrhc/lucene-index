467710122fce8345c53377638bcb0c0d636dd9d7
adrhc <adrhc@outlook.com>
2026-03-01 13:13:50 +0200

    SearchResultFilter

--- src/main/java/ro/go/adrhc/persistence/lucene/operations/search/SearchResultFilter.java
+++ src/main/java/ro/go/adrhc/persistence/lucene/operations/search/SearchResultFilter.java
@@ -2,6 +2,7 @@ package ro.go.adrhc.persistence.lucene.operations.search;
 
 import ro.go.adrhc.persistence.lucene.core.bare.read.ScoreDocAndValue;
 
+@FunctionalInterface
 public interface SearchResultFilter<T> {
 	boolean filter(ScoreDocAndValue<T> found);
 }
