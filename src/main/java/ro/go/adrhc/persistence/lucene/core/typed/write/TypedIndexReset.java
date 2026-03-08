2e1cf4585ce703c83c53b32183534ff2e4c8bdd8
adrhc <adrhc@outlook.com>
2026-03-08 12:31:01 +0200

    TypedIndexReset.reset: commit the changes

--- src/main/java/ro/go/adrhc/persistence/lucene/core/typed/write/TypedIndexReset.java
+++ src/main/java/ro/go/adrhc/persistence/lucene/core/typed/write/TypedIndexReset.java
@@ -1,10 +1,12 @@
 package ro.go.adrhc.persistence.lucene.core.typed.write;
 
+import lombok.extern.slf4j.Slf4j;
 import ro.go.adrhc.persistence.lucene.core.bare.write.DocIndexWriter;
 
 import java.io.IOException;
 import java.util.stream.Stream;
 
+@Slf4j
 public class TypedIndexReset<T> extends AbstractIndexWriter<T> {
 	public TypedIndexReset(TypedToDocumentConverter<T> toDocumentConverter,
 		DocIndexWriter indexWriter) {
@@ -18,6 +20,10 @@ public class TypedIndexReset<T> extends AbstractIndexWriter<T> {
 	}
 
 	public void reset(Stream<T> stateAfterReset) throws IOException {
+		log.info("\nresetting the index ...");
 		docIndexWriter.reset(toDocuments(stateAfterReset));
+		log.info("\ncommitting changes to the index ...");
+		docIndexWriter.commit();
+		log.info("\nIndex reset complete!");
 	}
 }
